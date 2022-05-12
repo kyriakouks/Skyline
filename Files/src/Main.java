261777138936ec56c13e72211ef294f27a25dc1b
Kyriakos Kyriakou <kyriakouks@csd.auth.gr>
2022-05-12 20:41:54 +0300

    added a file and an arraylist for X and Y

--- Files/src/Main.java
+++ Files/src/Main.java
@@ -1,16 +1,35 @@
-import java.io.IOException;
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
-    public static void main(String[] args){
-        String number = "";
-        try(BufferedReader in = new BufferedReader(new InputStreamReader(System.in))){
-            number = in.readLine();
+
+    public static void main(String[] args) {
+        ArrayList<String> X = new ArrayList<>();
+        ArrayList<Integer> Y = new ArrayList<>();
+
+        try {
+            File file = new File("src\\Input"); //remember to change to args[0]
+            BufferedReader in = new BufferedReader(new FileReader(file));
+            String next;
+            int arrayLength = Integer.parseInt(in.readLine());
+            while ((next = in.readLine()) != null){
+                //String[] splited = next.split(" ");
+                X.add(next);
+                //Y.add(Integer.parseInt(splited[1]));
+            }
         }catch (IOException e){
             System.out.println(e.getMessage());
         }
-        Integer.parseInt(number);
-        System.out.println(number);
+        Collections.sort(X);
+        for (String i : X){
+            System.out.println(i);
+        }
+
     }
+    
+
 }
